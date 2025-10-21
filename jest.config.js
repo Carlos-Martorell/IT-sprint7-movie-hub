d76cc3476c85b78aa7303164ad166691dd93ad3a
Carlos-Martorell <cmartorellotal@gmail.com>
2025-10-21 10:58:04 +0200

    chore: initial project setup with Angular 20, Jest and Tailwind

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['html', 'text', 'lcov'],
+  testMatch: ['**/*.spec.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@app/(.*)$': '<rootDir>/src/app/$1',
+    '^@core/(.*)$': '<rootDir>/src/app/core/$1',
+    '^@shared/(.*)$': '<rootDir>/src/app/shared/$1',
+    '^@features/(.*)$': '<rootDir>/src/app/features/$1'
+  }
+};
