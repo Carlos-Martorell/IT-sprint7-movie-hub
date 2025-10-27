dc6806e2c42bd2d63b091c30eea49b942c714d31
Carlos-Martorell <cmartorellotal@gmail.com>
2025-10-27 13:15:05 +0100

    feat: create movie detail component with routing logic

---
+++ src/app/features/movies/movie-detail/movie-detail.ts
@@ -0,0 +1,61 @@
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MovieService } from '@app/core/services/movies';
+
+@Component({
+  selector: 'app-movie-detail',
+  imports: [CommonModule],
+  templateUrl: './movie-detail.html',
+  styleUrl: './movie-detail.css'
+})
+export class MovieDetail implements OnInit {
+
+  private readonly route = inject(ActivatedRoute);
+  private readonly router = inject(Router);
+  private readonly movieService = inject(MovieService);
+
+  movie = signal<MovieDetail | null>(null);
+  loading = signal<boolean>(true);
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+
+    if (id) {
+      this.loadMovieDetail(Number(id));
+    } else {
+      // Si no hay ID, volver a la lista
+      this.router.navigate(['/movies']);
+    }
+  }
+
+loadMovieDetail(id: number): void {
+  this.loading.set(true);
+
+  this.movieService.getMovieById(id).subscribe({
+    next: (movie) => {
+      this.movie.set(movie);
+      this.loading.set(false);
+      console.log('Película cargada:', movie);
+    },
+    error: (error) => {
+      console.error('Error al cargar película:', error);
+      this.loading.set(false);
+      // Si hay error (404, película no existe), volver a la lista
+      this.router.navigate(['/movies']);
+    }
+  });
+}
+
+goBack(): void {
+  this.router.navigate(['/movies']);
+}
+
+getImageUrl(path: string | null, size: string = 'w500'): string {
+  if (!path) {
+    return 'https://via.placeholder.com/500x750?text=Sin+imagen';
+  }
+  return `https://image.tmdb.org/t/p/${size}${path}`;
+}
+
+}
